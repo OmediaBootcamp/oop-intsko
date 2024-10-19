2b56ad5731abcec6988d93e3ffef907dcbbb7780
1ntsko <intskirvelilashagiorgi@gmail.com>
2024-10-19 13:16:52 +0400

    Cylinder | Rectangle

---
+++ src/dev/omedia/Cuboid.java
@@ -0,0 +1,23 @@
+package dev.omedia;
+
+public class Cuboid extends Rectangle {
+    private final double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+}
+
